fa288de3fc2ce9e4b6ee6587302dd4611d7af4b6
Abel Lamas <abel.synyster@gmail.com>
2022-09-01 15:45:50 -0300

    practica_1 finalizada

--- practica_1/Practica_1_SyS.py
+++ practica_1/Practica_1_SyS.py
@@ -2,33 +2,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#%%
-zc1 = 2+3j
-zc2 = complex(2,3)
-print('¿zc1 = zc2?', (zc1 == zc2))
-zc1_real = np.real(zc1)
-
-zc1_real = np.real(zc1)
-zc1_imag = np.imag(zc1)
-zc1_angle= np.angle(zc1)
-zc1_conj = np.conj(zc1)
-
-print(zc1)
-print('parte real: ', zc1_real)
-print('parte imaginaria: ', zc1_imag)
-print('angulo en radianes: ', zc1_angle)
-print('complejo conjugado: ', zc1_conj)
-
-#%%
-# ejercicio 1.01
-# a traves de una funcion de numpy
+#%% ejercicio 1.01
+zc1 = 1+1j
+# a través de una función de numpy
 zc1_module = np.absolute(zc1)
 print(zc1_module)
 # through a method of a complex object
 print(zc1.__abs__())
 
-#%%
-# ejercicio 1.02
+#%% ejercicio 1.02
 z1 = 4 + 3j
 z2 = 2 + 2j
 
@@ -38,45 +20,81 @@ mult = z1*z2
 div = z1/z2
 print('Suma: ', suma)
 print('Resta: ', resta)
-print('Multiplicacion: ', mult)
+print('Multiplicación: ', mult)
 print('Division: ', div)
 
-
-#%% 
-c = 1 + 2j
-print('e^c=', np.exp(c))
-print('log_natural(c)=', np.log(c))
-print('log10(c)', np.log10(c))
-
-#%%
-#Logaritmo de cero
-print('log(0)', np.log(0))
-print('log(inf)', np.log(np.inf))
-
 #%% ejercicio 1.03
-c = np.array([[1, 5, 2],[3, 4, 8]])
+c = np.array([[1, 5, 2], [3, 4, 8]])
 print(c.shape)
 
 #%% ejercicio 1.04
-print(c[:,1])
+print(c[:, 1])
 #%% ejercicio 1.05
-a = np.array([1,2,3,4])
-#%%
-b = a.reshape(2,2)
+a = np.array([1, 2, 3, 4])
+#%% ejercicio 1.06
+b = a.reshape(2, 2)
 print(b)
 #%% ejercicio 1.6
 
-T=0.01
+T = 0.01
 time = np.arange(-1, 1+T, T)
 x_values = []
 for t in time:
     x = 2*np.sin(2*np.pi*5*t)
-    if x<0:
+    if x < 0:
         x_values.append(-x)
     else:
         x_values.append(x)
-plt.plot(time,x_values)
-plt.grid()        
+plt.plot(time, x_values)
+plt.grid()
+#%% ejercicio 1.07
 
-        
+numero_7 = 34
+if numero_7 % 2 == 0:
+    print(f'el número {numero_7} es par')
+else:
+    print(f'el número {numero_7} es impar')
+
+#%% ejercicio 1.08
+
+numeros = [1, 2, 3, 4, 5]
+for numero in numeros:
+    print(numero)
+
+#%% ejercicio 1.09
+numero_primo = 37
+sucesion = np.arange(2, numero_primo + 1, 1)
+divisores = []
+
+for n in sucesion:
+    if numero_primo % n == 0:
+        divisores.append(n)
+
+if divisores[0] == numero_primo:
+    print('es primo')
+else:
+    print('no es primo')
+#%% ejercicio 1.10
+lista = [1, 4, 5, 2, 3]
+lista.reverse()
+print(lista)
+#%% ejercicio 1.11
+
+'''
+★★★★☆ - 1.11) Dado un numero entero positivo de entrada n, 
+escribir la serie de fibonacci hasta el n-ésimo elemento
+'''
+
+n = int(input())
+f_1 = 0
+f_2 = 1
+f_n = 1
+i = 0
+
+while i < n:
+    print(f_n)
+    f_n = f_1 + f_2
+    f_1 = f_2
+    f_2 = f_n
+    i = i + 1
 
